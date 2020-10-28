237a5b8efbcf8c9c3289a25fab3474c055b64bcb
S4UR000N <backtrack.grey@gmail.com>
2020-10-28 23:07:31 +0100

    3. Added draft of Smith Gotoh Similarity algorithm

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -1,15 +1,22 @@
 <?php
-// src/Controller/LuckyController.php
+
+// src/Controller/IndexController.php
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 
+use App\Service\SmithWatermanGotohService;
+use App\Service\SmithWatermanMatchMismatchService;
+
+
 class IndexController extends AbstractController
 {
     public function indexGET(): Response
     {
-        return $this->render('index.html.twig');
+        $o = new SmithWatermanGotohService();
+        $o = $o->compare("LEGENDARY","LEGiNDcRY");
+        return $this->render('index.html.twig', ['o' => $o]);
     }
 
     public function indexPOST(): Response
